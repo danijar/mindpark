4bdb98382619b10fe5a030892436cac8ce834a8f
Danijar Hafner <mail@danijar.com>
2016-07-13 16:56:37 +0200

    Add plotting class for std charts

--- vizbot/benchmark.py
+++ vizbot/benchmark.py
@@ -1,15 +1,62 @@
-import time
-import os
 import argparse
 import collections
+import itertools
+import os
+import time
 import numpy as np
 import matplotlib.pyplot as plt
+import gym
 import vizbot.agent
-from vizbot.utility import ensure_directory
+from vizbot.utility import ensure_directory, AttrDict
 
 
-def read_comparison(self, directory):
-    benchmarks = [x for x in os.listdir(directory) if os.path.isdir(x)]
+class ComparisonPlot:
+
+    COLORS = ('green', 'blue', 'red', 'yellow')
+
+    LABEL_ARGS = AttrDict(
+        loc='best', frameon=False, fontsize='medium', labelspacing=0)
+
+    def __init__(self, ncols, title):
+        self._ncols = ncols
+        self._fig = plt.figure(figsize=(12, 4))
+        self._fig.suptitle(title, fontsize=16)
+        self._index = 1
+
+    def add(self, title, xlabel, ylabel, **lines):
+        lines = sorted(lines.items(), key=lambda x: -x[1].sum())
+        ax = self._next_plot()
+        ax.set_title(title)
+        ax.set_xlabel(xlabel)
+        ax.set_ylabel(ylabel)
+        for index, (label, line) in enumerate(lines):
+            color = self.COLORS[index]
+            self._plot(ax, label, line, color)
+        ax.legend(**self.LABEL_ARGS)
+
+    def save(self, filepath):
+        self._fig.tight_layout(rect=[0, 0, 1, .93])
+        self._fig.savefig(filepath, dpi=300)
+
+    def _next_plot(self):
+        if self._index > self._ncols:
+            raise RuntimeError
+        ax = self._fig.add_subplot(1, self._ncols, self._index)
+        self._index += 1
+        return ax
+
+    def _plot(self, ax, label, line, color):
+        means = np.cumsum(line.mean(axis=0))
+        stds = line.std(axis=0)
+        area = np.arange(len(means)), means - stds, means + stds
+        ax.fill_between(*area, color=color, alpha=0.15)
+        ax.plot(means, label=label, color=color)
+
+
+def read_comparison(directory):
+    benchmarks = os.listdir(directory)
+    benchmarks = [os.path.join(directory, x) for x in benchmarks]
+    benchmarks = [x for x in benchmarks if os.path.isdir(x)]
     rewards = collections.defaultdict(dict)
     for benchmark in benchmarks:
         env, agent = os.path.basename(benchmark).rsplit('-', 1)
@@ -19,29 +66,20 @@ def read_comparison(self, directory):
     return rewards
 
 
-def plot_comparison(self, directory, rewards):
-    fig, ax = plt.subplots(ncols=len(rewards), figsize=(12, 4))
-    for index, env in enumerate(sorted(rewards.keys())):
-        for agent in rewards[env]:
-            means = rewards[env][agent].mean(axis=0)
-            ax[index].plot(means, label=agent)
-    fig.savefig(os.path.join(directory, 'comparison.pdf'), dpi=300)
-
-
 def train(directory, env, agent, episodes):
     """
     Train an agent in an environment and store its gym monitoring. Return
     rewards and training duration.
     """
-    state, reward, done = env.reset(), 0, False
     ensure_directory(directory)
     env.monitor.start(directory)
     start, rewards = time.time(), []
-    for episode in episodes:
+    state, reward, done = env.reset(), 0, False
+    for episode in range(episodes):
         rewards.append(0)
         while not done:
             action = agent(state, reward)
-            state, reward, done = env.step(action)
+            state, reward, done, _ = env.step(action)
             rewards[-1] += reward
     duration = time.time() - start
     return rewards, duration
@@ -61,8 +99,8 @@ def benchmark(root, env, agent, repeats, episodes):
         rewards.append(reward)
         durations.append(duration)
     rewards, durations = np.array(rewards), np.array(durations)
-    np.save(os.path.join(directory, 'rewards.npy'), rewards)
-    np.save(os.path.join(directory, 'durations.npy'), durations)
+    np.save(os.path.join(root, 'rewards.npy'), rewards)
+    np.save(os.path.join(root, 'durations.npy'), durations)
     return rewards, durations
 
 
@@ -98,7 +136,7 @@ def parse_args():
         '-n', '--episodes', type=int,
         default='10')
     parser.add_argument(
-        '-d', '--directory',
+        '-o', '--directory',
         default='~/experiment/gym')
     parser.add_argument(
         '-e', '--experiment',
@@ -109,13 +147,17 @@ def parse_args():
 
 def main():
     args = parse_args()
-    timestamp = time.strftime('%Y-%m-%dT%H-%M-%', time.gmtime())
+    timestamp = time.strftime('%Y-%m-%dT%H-%M-%S', time.gmtime())
     directory = os.path.abspath(os.path.expanduser(args.directory))
-    directory = os.path.join(directory, '{}-{}'.format(timestamp, args.name))
+    directory = os.path.join(
+        directory, '{}-{}'.format(timestamp, args.experiment))
     print('Start experiment', directory)
     compare(directory, args.envs, args.agents, args.repeats, args.episodes)
     rewards = read_comparison(directory)
-    plot_comparison(directory, rewards)
+    plot = ComparisonPlot(len(rewards), os.path.basename(directory))
+    for env, agents in rewards.items():
+        plot.add(env, 'Training Episode', 'Cumulative Reward', **agents)
+    plot.save(os.path.join(directory, 'comparison.pdf'))
 
 
 if __name__ == '__main__':
